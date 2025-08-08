0bf90f45e1c774969b2126debfae98c75aae0b3f
Oliver Soeser <o.soeser@icloud.com>
2025-08-08 14:46:27 +0200

    iauto structure

---
+++ src/Iris/ProofMode/Tactics/Auto.lean
@@ -0,0 +1,30 @@
+/-
+Copyright (c) 2025 Lars König. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Oliver Soeser
+-/
+import Iris.ProofMode.Tactics.Basic
+
+namespace Iris.ProofMode
+open Lean Elab Tactic Meta Qq BI Std
+
+variable {prop : Q(Type u)} (bi : Q(BI $prop)) in
+partial def iAutoCore
+    {P} (hyps : Hyps bi P) (Q : Q($prop))
+    (k : ∀ {P}, Hyps bi P → (Q : Q($prop)) → MetaM Q($P ⊢ $Q)) :
+    MetaM (Q($P ⊢ $Q)) := do
+  throwError "iauto: not implemented"
+
+elab "iauto" : tactic => do
+  let (mvar, { prop, bi, hyps, goal, .. }) ← istart (← getMainGoal)
+
+  mvar.withContext do
+    let goals ← IO.mkRef #[]
+    let pf ← iAutoCore bi hyps goal fun {P} hyps goal => do
+      let m : Q($P ⊢ $goal) ← mkFreshExprSyntheticOpaqueMVar <|
+        IrisGoal.toExpr { prop, bi, hyps, goal, .. }
+      goals.modify (·.push m.mvarId!)
+      pure m
+
+    mvar.assign pf
+    replaceMainGoal (← goals.get).toList
