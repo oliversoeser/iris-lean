561a7978b2a49ab4a93a5c65b96fcf9d7ebc5740
Oliver Soeser <hello@oliversoeser.com>
2026-07-11 21:25:22 +0200

    bi mono instances setup

---
+++ Iris/Iris/BI/Lib/Monotone.lean
@@ -0,0 +1,41 @@
+/-
+Copyright (c) 2026 Oliver Soeser. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Oliver Soeser
+-/
+module
+
+public import Iris.BI.Lib.Fixpoint
+
+@[expose] public section
+
+namespace Iris
+open BI OFE
+
+instance monotone_constant [BI PROP] [OFE A] : BIMonoPred
+    (λ_ : A → PROP => λ_ : A => P) where
+  mono_pred {_ _ _ _} := by
+    iintro _ %_ HP
+    iexact HP
+  mono_pred_ne := by infer_instance
+
+instance monotone_id [BI PROP] [OFE A] : BIMonoPred
+    (λΦ : A → PROP => Φ) where
+  mono_pred {_ _ _ _} := by
+    iintro #H
+    iexact H
+  mono_pred_ne := by infer_instance
+
+instance monotone_later [BI PROP] [OFE A] (F : (A → PROP) → A → PROP) [hf : BIMonoPred F] :
+    BIMonoPred (λΦ : A → PROP => λx : A => iprop(▷F Φ x)) where
+  mono_pred {Φ Ψ h₁ h₂} := by
+    iintro #H1 %x H2
+    inext
+    iapply @hf.mono_pred Φ Ψ h₁ h₂
+    iapply H1
+    iexact H2
+  mono_pred_ne {Φ h} := by
+    constructor
+    intro n x₁ x₂ hneq
+    have h₁ := (@hf.mono_pred_ne Φ h).ne hneq
+    exact @(@later_ne PROP _).ne n (F Φ x₁) (F Φ x₂) h₁
