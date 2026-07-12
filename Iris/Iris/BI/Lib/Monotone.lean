93d3b0804c82712d34346a7f56eb65a31652218a
Oliver Soeser <hello@oliversoeser.com>
2026-07-12 06:48:13 +0200

    persistently mono

--- Iris/Iris/BI/Lib/Monotone.lean
+++ Iris/Iris/BI/Lib/Monotone.lean
@@ -26,8 +26,22 @@ instance monotone_id [BI PROP] [OFE A] : BIMonoPred
     iexact H
   mono_pred_ne := by infer_instance
 
+instance monotone_persistently [BI PROP] [OFE A] (F : (A → PROP) → A → PROP) [hf : BIMonoPred F] :
+    BIMonoPred (λΦ : A → PROP => λx : A => iprop(<pers> F Φ x)) where
+  mono_pred {Φ Ψ h₁ h₂} := by
+    iintro #H1 %x #H2
+    imodintro
+    iapply @hf.mono_pred Φ Ψ h₁ h₂
+    iapply H1
+    iexact H2
+  mono_pred_ne {Φ h} := by
+    constructor
+    intro n x₁ x₂ hneq
+    have h' := (@hf.mono_pred_ne Φ h).ne hneq
+    exact persistently_ne.ne h'
+
 instance monotone_later [BI PROP] [OFE A] (F : (A → PROP) → A → PROP) [hf : BIMonoPred F] :
-    BIMonoPred (λΦ : A → PROP => λx : A => iprop(▷F Φ x)) where
+    BIMonoPred (λΦ : A → PROP => λx : A => iprop(▷ F Φ x)) where
   mono_pred {Φ Ψ h₁ h₂} := by
     iintro #H1 %x H2
     inext
@@ -37,5 +51,5 @@ instance monotone_later [BI PROP] [OFE A] (F : (A → PROP) → A → PROP) [hf 
   mono_pred_ne {Φ h} := by
     constructor
     intro n x₁ x₂ hneq
-    have h₁ := (@hf.mono_pred_ne Φ h).ne hneq
-    exact @(@later_ne PROP _).ne n (F Φ x₁) (F Φ x₂) h₁
+    have h' := (@hf.mono_pred_ne Φ h).ne hneq
+    exact later_ne.ne h'
